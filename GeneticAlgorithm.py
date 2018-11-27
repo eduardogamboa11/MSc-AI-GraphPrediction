9edc6771ab7e9aee892fc9db8e1d99c3a8141e02
eduardogamboa11 <eduardo_Gamboa_95@hotmail.com>
2018-11-27 02:25:51 -0600

    update

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -1,8 +1,9 @@
 import numpy
 import math
 import random
-from matplotlib import pyplot as plt
-
+from random import randint as rdi
+from matplotlib import pyplot as pl
+from fuzzyNetwork import get_aptitude
 
 def get_population(alleles, chromosomes, individuals):    
     population = numpy.random.randint(2**alleles, size=(individuals, chromosomes))
@@ -13,15 +14,15 @@ def get_population(alleles, chromosomes, individuals):
 def tournament(population, individuals, number_of_contestants):
     contestants = numpy.random.randint(individuals, size=number_of_contestants)
     winner = population[min(contestants)]
-    
+
     return winner
 
 
 def numbers_to_string(individual):
-    binary_string = ''.join([bin(item) for item in s]).replace('b','')
+    binary_string = ''.join(["{:08b}".format(item) for item in individual])
 
     return binary_string
-    
+
 
 def get_child(father, mother, population, individuals):
     cut_distance = random.randint(1,len(father)-1)
@@ -53,51 +54,16 @@ def string_to_numbers(alleles, chromosomes, individual):
 
     for i in range(chromosomes):
         numbers[i] = int(individual[i*alleles:(i+1)*alleles], 2)
-    
+
     return numbers
 
 
-def get_aptitude(individual):
-    A, B, C, D, E, F, G, H = 25, 123, 13, 15, 0.08, 80, 16, 15
-
-    #Avoid division by 0
-    for i in range(len(individual)):
-        if individual[i] == 0:
-            individual[i] = 1
-    A1, B1, C1, D1, E1, F1, G1, H1 = individual
-
-    aptitude = 0
-    #calculate difference from target with individual for every x(t) 
-    for t in range(500):
-        x = (A * math.exp(-t/B)) * (C * math.sin(t/D)) + (E * math.exp(t/F)) * (G * math.cos(t/H))
-        y = (A1 * math.exp(-t/B1)) * (C1 * math.sin(t/D1)) + ((E1/500) * math.exp(t/F1)) * (G1 * math.cos(t/H1))
-        aptitude += abs(x - y)    
-
-    return aptitude
-
-
-def graph_comparison(individual):
-    #Use to compare graphs between target and individual
-    A, B, C, D, E, F, G, H = 25, 123, 13, 15, 0.08, 80, 16, 15
-
-    for i in range(len(individual)):
-        if individual[i] == 0:
-            individual[i] = 1
-    A1, B1, C1, D1, E1, F1, G1, H1 = individual
-
-    x = [((A * math.exp(-t/B)) * (C * math.sin(t/D)) + (E * math.exp(t/F)) * (G * math.cos(t/H))) for t in range(500)]
-    y = [((A1 * math.exp(-t/B1)) * (C1 * math.sin(t/D1)) + ((E1/500) * math.exp(t/F1)) * (G1 * math.cos(t/H1))) for t in range(500)]
-
-    plt.plot(x,"blue",y,"red")
-    plt.show()
-
-
 alleles = 8
-chromosomes = 8 
-individuals = 2000
-mutation_probability = 0.015
-generations = 15
-number_of_contestants = 20
+chromosomes = 5*4 
+individuals = 1500
+mutation_probability = 0.05
+generations = 100
+number_of_contestants = int(individuals*.05)
 
 population = get_population(alleles, chromosomes, individuals)
 
@@ -116,11 +82,17 @@ for gen in range(generations):
         new_population.append([child_1, get_aptitude(child_1)])
         new_population.append([child_2, get_aptitude(child_2)])
 
-    results = sorted(new_population, key=lambda x:(x[1]))
-
+    results = sorted(new_population, key=lambda x:(x[1][0]))
+    
     print('gen:', gen)
-    print(results[0])
-    graph_comparison(results[0][0])
+    print(results[0][1])
+    
+    input_plot = [17.53, 16.94, 8.84, 12.13, 22.01, 12.65, 12.67,23.98,14.13,11.76,10.33,0,0,0]
+
+    
 
     for individual in range(individuals):
         population[individual]=results[individual][0]
+
+pl.plot(input_plot, "blue", results[0][1][1], "red")
+pl.show()
--- fuzzyNetwork.py
+++ fuzzyNetwork.py
@@ -2,26 +2,51 @@ from matplotlib import pyplot as pl
 import numpy
 import math
 
+def calculate_gaussian(input_plot, average, standard_deviation):
+    if standard_deviation < 10:
+        standard_deviation = 10
+    
+    gaussian = [math.exp(-(((i)-(average*14/255))**2)/(2*((standard_deviation/25)**2))) for i in range(14)]
 
-average1,average2,average3 = -10, 0, 10
-standard_deviation1,standard_deviation2,standard_deviation3 = 4.25, 0.5, 4.25
-p1,p2,p3 = 1, 2, 1
-q1,q2,q3 = 0, 0, 0
+    return gaussian
 
-x = [i/10 for i in range(-100,101)]
-numbers = [i for i in range(200)]
 
-gaussian1 = [math.exp(-(((x[i]-average1)**2)/(2*(standard_deviation1**2)))) for i in numbers]
-gaussian2 = [math.exp(-(((x[i]-average2)**2)/(2*(standard_deviation2**2)))) for i in numbers]
-gaussian3 = [math.exp(-(((x[i]-average3)**2)/(2*(standard_deviation3**2)))) for i in numbers]
+def calculate_sp(input_plot, gaussian, weights, bias):
+    sp = [(gaussian[i]*((weights/5)*i))+(bias/5) for i in range(14)]    
+    
 
-sp1 = [gaussian1[i]*((p1*x[i])+q1) for i in numbers]
-sp2 = [gaussian2[i]*((p2*x[i])+q2) for i in numbers]
-sp3 = [gaussian3[i]*((p3*x[i])+q3) for i in numbers]
+    return sp
 
-a = [sp1[i] + sp2[i] + sp3[i] for i in numbers]
-b = [gaussian1[i] + gaussian2[i] + gaussian3[i] for i in numbers]
-y = [a[i]/b[i] for i in numbers]
 
-pl.plot(x, 'blue',y, 'red')
-pl.show()
+def get_output(gaussians, sp_list):   
+    b = list(map(sum, [[gaussians[i][j] for i in range(len(gaussians))] for j in range(len(gaussians[0]))]))
+    a = list(map(sum, [[sp_list[i][j] for i in range(len(sp_list))] for j in range(len(sp_list[0]))]))
+
+    
+    output_plot = [a[i]/b[i] for i in range(int(len(a)))]
+
+    return output_plot
+
+
+def get_aptitude(individual):
+    #individual = [av1,sd1,p1,q1,av2,sd2,p2,q2,....avn,sdn,pn,qn]
+    input_plot = [17.53, 16.94, 8.84, 12.13, 22.01, 12.65, 12.67, 23.98, 14.13, 11.76, 10.33, 0, 0, 0]
+
+    sp_list = []
+    gaussians = []
+    for i in range(int(len(individual)/4)):
+        gaussians.append(calculate_gaussian(input_plot, individual[i*4], individual[(i*4)+1])) 
+
+
+    #pl.plot(gaussians[0], 'green', gaussians[1], 'blue', gaussians[2], 'red', gaussians[3], 'yellow', gaussians[4], 'black')
+    #pl.show()
+
+    for gaussian in gaussians:
+        sp_list.append(calculate_sp(input_plot, gaussian, individual[(i*4)+2], individual[(i*4)+3]))
+
+    output_plot = get_output(gaussians, sp_list)
+    aptitude = 0
+    for i in range(len(input_plot)-3):
+        aptitude += abs(input_plot[i] - output_plot[i])
+    
+    return aptitude, output_plot
